3c650b726dd011dcc948783d3ca2b6ac84d35618
Thorbjørn Ravn Andersen <thorbjoern@gmail.com>
2009-09-07 15:07:21 +0200

    Source from http://sourceforge.net/projects/jsocks/files/jsocks/1.01/jsocks_code1.01.zip/download

---
+++ jsocks_code1.01/test/UPSOCKS.java
@@ -0,0 +1,40 @@
+package test;
+import socks.*;
+import socks.server.*;
+import java.net.Socket;
+
+/** Test file for UserPasswordAuthentictor */
+
+public class UPSOCKS implements UserValidation{
+    String user, password;
+
+    UPSOCKS(String user,String password){
+       this.user = user;
+       this.password = password;
+    }
+
+    public boolean isUserValid(String user,String password,Socket s){
+       System.err.println("User:"+user+"\tPassword:"+password);
+       System.err.println("Socket:"+s);
+       return (user.equals(this.user) && password.equals(this.password));
+    }
+
+    public static void main(String args[]){
+        String user, password;
+
+        if(args.length == 2){
+          user = args[0];
+          password = args[1];
+        }else{
+          user = "user";
+          password = "password";
+        }
+
+        UPSOCKS us = new UPSOCKS(user,password);
+        UserPasswordAuthenticator auth = new UserPasswordAuthenticator(us);
+        ProxyServer server = new ProxyServer(auth);
+
+        server.setLog(System.out);
+        server.start(1080);
+    }
+}
